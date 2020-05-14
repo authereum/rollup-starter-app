c08ced515e5f78ea55092719ab0ca0b29630f45e
Miguel Mota <miguelmota2@gmail.com>
2020-05-14 09:27:32 -0700

    Add authereum sdk example

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import update from './update.js';
+import Authereum from 'authereum'
+
+const authereum = new Authereum('kovan')
+const provider = authereum.getProvider()
+console.log('provider', provider)
 
 // even though Rollup is bundling all your files together, errors and
 // logs will still point to your original source modules
